2dd1ac1c4191dc0339aa9a233f5a10c05ae1db7f
akza <itsakza07@gmail.com>
2022-06-06 16:07:24 +0530

    Remove unused [spec.ts] , Improve Middleware log

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -4,7 +4,12 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export class logger implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log(`Incoming request to API${req.url}`);
+    console.log(`[Incoming-middleware] Request to ${req.route.path}`);
+    console.log(
+      `[Incoming-middleware] User-Agent : `,
+      req.headers['user-agent'],
+    );
+    console.log('[Outgoing-middleware]', res.req.body);
 
     next();
   }
